ee00c4069443ee3ce664985c322048b006edae8e
21Alul21 <augustine.alul@yahoo.com>
2023-04-03 11:46:15 -0700

    more class update

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+""" this module basically 
+    defines an empty class.
+"""
 class Rectangle:
     """ this is an empty class
-        that defines a 'rectangle'
+        that defines a 'rectangle'.
     """
     pass
 rectangle = Rectangle()
+print(rectangle.__doc__)
 
