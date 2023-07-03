75b2405ac508f8aff14404b26764c29e3556a4d7
21Alul21 <augustine.alul@yahoo.com>
2023-07-03 21:21:25 +0000

    update

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+"""
+
+module that contains an empty class
+"Rectangle"
+that defines a rectangle.
+
+"""
+
+class Rectangle:
+    pass
+ 
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,27 @@
+"""
+module that contains a class that 
+has two instant variables 
+with properties set using the @property and
+@setter decorators.
+
+"""
+
+class Rectangle:
+"""retrieves the rectangle parameters and set it"""
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return _self.width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(self.width, int):
+            raise TypeError("width must be an integer")
+        if self.width < 0:
+            raise ValueError("width must be >= 0")
+        value = _self.width
+        return value   
+
