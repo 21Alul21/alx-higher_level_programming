ee9dfbbbcc80a568192709ea6cb36e707bef41f1
21Alul21 <augustine.alul@yahoo.com>
2023-07-10 15:51:31 -0700

    update

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -19,7 +19,4 @@ def is_kind_of_class(obj, a_class):
 
     """
 
-    if isinstance(obj, a_class):
-    return True
-    else:
-    return False
+    return isinstance(obj, a_class):
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -18,7 +18,7 @@ def inherits_from(obj, a_class):
 
     """
 
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return False
     else:
     return isinstance(obj, a_class)
