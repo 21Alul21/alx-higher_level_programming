c4f93e641e263638a05e547b37a51515eb8031bd
21Alul21 <augustine.alul@yahoo.com>
2023-07-12 12:13:59 -0700

    update

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This module contains a function 
+that reads the content of a text file
+"""
+
+
+def read_file(filename=""):
+    """ Function that prints ou
+        t the content of a text file 
+    """
+
+    with open("filename", mode="r", encoding="utf-8") as tf:
+        content = tf.read()
+        print(content) 
+
