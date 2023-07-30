79ddca61e5a65ff493fc544617cf72fed2f347b2
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2023-07-30 19:34:04 +0100

    Update 0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """
-
 module that contains an empty class
 Rectangle
 that defines a rectangle..
-
 """
 
 
@@ -13,8 +11,5 @@ class Rectangle:
         this is an empty
         class that defines a 
         rectangle
-
     """
-
     pass
- 
