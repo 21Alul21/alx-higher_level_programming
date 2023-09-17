e7761bb34ca49ba3876fd0a510937c11f755bb87
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2023-09-17 22:41:41 +0100

    Update 0-select_states.py
    adjusted indentations

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,11 +9,11 @@ import sys
 
 
 if __name__ == "__main__":      
-        conn = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],  port=3306)
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-        result = cur.fetchall()
-        for line in result:
-            print(line)
-        cur.close()
-        conn.close()
+    conn = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],  port=3306)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    result = cur.fetchall()
+    for line in result:
+        print(line)
+    cur.close()
+    conn.close()
